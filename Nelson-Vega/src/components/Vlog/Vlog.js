b2d819595d5e9d981733583fc6303803c7a0b41f
Harjeet15 <48899858+Harjeet15@users.noreply.github.com>
2019-10-12 22:27:59 +0530

    Update Vlog.js

--- Nelson-Vega/src/components/Vlog/Vlog.js
+++ Nelson-Vega/src/components/Vlog/Vlog.js
@@ -3,42 +3,42 @@ import './Vlog.css'
 import VLog from '../VlogCard'
 const Vlog = () => {
   return (
-    <div className="VlogContiner" id="Vlog">
-       <div style={{display: 'flex',
-       flexDirection: 'column',
-       backgroundColor:'#f2f1f1',
-       height:'100%'}}>
-       <div className="VlogHeadingText" style={style.Heading}>
-       <h3 style={{margin:0,fontSize:'2.2rem',color:'#d5b004'}}>FEATURED VIDEOS FROM THE</h3>
-       <h1 className="HeadVlog">VLog</h1>
-       </div>
-       <div className="SlidervLOG">
-       {['Huma.jpg','vlog.png','vlog_two.png'].map((data,index)=>{
+    // <div className="VlogContiner" id="Vlog">
+       // <div style={{display: 'flex',
+       // flexDirection: 'column',
+       // backgroundColor:'#f2f1f1',
+       // height:'100%'}}>
+       // <div className="VlogHeadingText" style={style.Heading}>
+       // <h3 style={{margin:0,fontSize:'2.2rem',color:'#d5b004'}}>FEATURED VIDEOS FROM THE</h3>
+       // <h1 className="HeadVlog">VLog</h1>
+       // </div>
+       // <div className="SlidervLOG">
+       {/* ['Huma.jpg','vlog.png','vlog_two.png'].map((data,index)=>{
          return <div key={index.toString()}  style={{width: '100%'}}>
               <div style={{margin:'1rem'}}>
               <VLog ImageName={data}/>
               </div>
                    
               </div>
-       })
+       }) */
       }
-       </div>
-       <div className="ImageStyle">
-         {['Huma.jpg','vlog.png','vlog_two.png'].map((data,index)=>{
+       // </div>
+       // <div className="ImageStyle">
+         {/* ['Huma.jpg','vlog.png','vlog_two.png'].map((data,index)=>{
          return <div 
          key={index.toString()}
           style={{width:'30%',
           height:'100%'}}>
            <VLog ImageName={data}/>
            </div>
-         })
+         }) */
          }
-       </div>
-       <div style={{height:'30vh',display:'flex',justifyContent:'center',alignItems:'center'}}>
-       <button className="VlogButtonStyle">Watch More</button>
-       </div>
-       </div>
-    </div>
+       // </div>
+       // <div style={{height:'30vh',display:'flex',justifyContent:'center',alignItems:'center'}}>
+       // <button className="VlogButtonStyle">Watch More</button>
+       // </div>
+       // </div>
+    // </div>
   )
 }
 const style={
